0cfe846cc69a08c6ee50673bb32451ebb7134ca3
Vflash-APP <133582761+Vflash-APP@users.noreply.github.com>
2023-05-15 13:40:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,3 @@ $("#menu").click(function(){
   $("#mainscreen").toggleClass("movemaincontainer");
 });
 
-fetch('https://vflashapp.000webhostapp.com/vflash.php')
-  .then(response => response.text())
-  .then(data => {
-    console.log(data); // Display the retrieved data
-    // Do whatever you want with the data here
-  })
-  .catch(error => {
-    console.log('An error occurred:', error);
-  });
